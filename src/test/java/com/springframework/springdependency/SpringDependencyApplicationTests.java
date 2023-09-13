8d1e198ac05ce5decd7d60d27d8baa1289119668
Sergej Ebers <sergej.ebers@infoteam.de>
2023-09-13 11:28:14 +0200

    init

---
+++ src/test/java/com/springframework/springdependency/SpringDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.springdependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
